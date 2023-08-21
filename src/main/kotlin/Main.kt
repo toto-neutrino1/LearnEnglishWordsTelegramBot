257a430550adf28d0f1b694c45ca960b48cac0dd
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-22 01:30:03 +0300

    TB-3 menu start page and statistics creation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,13 +10,16 @@ fun main() {
         val word = Word(
             original = stringElem[0],
             translate =  stringElem[1],
-            correctAnswersCount = stringElem[2].toIntOrNull() ?: 0)
+            correctAnswersCount = stringElem[2].toIntOrNull() ?: 0
+        )
         dictionary.add(word)
     }
 
     for (word in dictionary) {
         println(word)
     }
+
+    startMenu(dictionary = dictionary, learningThreshold = 3)
 }
 
 data class Word(
@@ -24,3 +27,25 @@ data class Word(
     val translate: String,
     var correctAnswersCount: Int = 0
 )
+
+fun startMenu(dictionary: MutableList<Word>, learningThreshold: Int) {
+    while (true) {
+        println("\nМеню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+        println(
+            when (readln().toIntOrNull()) {
+                1 -> "учим слова"
+                2 -> getStatistics(dictionary, learningThreshold)
+                0 -> break
+                else -> "Ввод данных некорректный!"
+            }
+        )
+    }
+}
+
+fun getStatistics(dictionary: MutableList<Word>, learningThreshold: Int): String {
+    val numOfAllWords = dictionary.size
+    val numOfLearnedWords = dictionary.filter { it.correctAnswersCount >= learningThreshold }.size
+    val learnedPercent = 100 * numOfLearnedWords / numOfAllWords
+
+    return "Выучено $numOfLearnedWords из $numOfAllWords слов | $learnedPercent%"
+}
