6b2f142f2a39f0e3c52240d0b2539ec05d368f27
toto-neutrino1 <tolyan7007@gmail.com>
2023-08-21 04:01:09 +0300

    TB-2 Word class creation, string parsing (split)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,25 @@ import java.io.File
 
 fun main() {
     val wordsFile = File("words.txt")
+    val dictionary: MutableList<Word> = mutableListOf()
 
     val fileLines = wordsFile.readLines()
     for (line in fileLines) {
-        println(line)
+        val stringElem = line.split("|")
+        val word = Word(
+            original = stringElem[0],
+            translate =  stringElem[1],
+            correctAnswersCount = stringElem[2].toIntOrNull() ?: 0)
+        dictionary.add(word)
+    }
+
+    for (word in dictionary) {
+        println(word)
     }
 }
+
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int? = 0
+)
